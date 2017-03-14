e797175a3bd1c68946ab47e6afc5dffa546a36b1
craigholland <craig_holland@hotmail.com>
2017-03-13 23:20:04 -0700

    Log class

--- sys/sys_error.py
+++ sys/sys_error.py
@@ -3,18 +3,21 @@
 import os
 import logging
 import enum
+import inspect
 
 _COMMON_ERROR_TYPES = frozenset(
     ['AttributeError',
      'TypeError',
      'ValueError'])
 
-_ERROR_TYPES = frozenset(_COMMON_ERROR_TYPES + [
+_ERROR_TYPES = frozenset(list(_COMMON_ERROR_TYPES) + [
     'ArithmeticError',
     'AssertionError'
     'BaseException',
     'BufferError',
     'FloatingPointError'])
+
+_LogNames = ['USERLOG', 'SYSLOG']
 
 def _getUserIP():
   return 'hollandc', '192.168.0.1'
@@ -30,22 +33,29 @@ class LogTypes(enum.Enum):
 class Log(object):
   _format = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
   logging.basicConfig(format=_format)
+  _initialized = False
 
-  USERLOG = logging.getLogger('USERLOG')
-  SYSLOG = logging.getLogger('SYSLOG')
+  @classmethod
+  def _createLogs(cls):
+      for n in _LogNames:
+          setattr(cls, n, logging.getLogger(n))
+      cls._initialized=True
 
   def __init__(self):
     self.user, self.client_ip = _getUserIP()
     self.extra = {'user': self.user, 'clientip': self.client_ip}
-
+    if not self._initialized:
+        self._createLogs()
 
   def _GetLog(self, log):
-    if log in self.__members__
-    self.USERLOG
+    if log in _LogNames:
+        return getattr(self, log)
 
 
 
-def Alert(err_msg, err_type, do_raise=False, do_log=True, log_type=LogTypes.INFO):
-    if err_type in _ERROR_TYPES:
-      if do_log:
+
+
+# def Alert(err_msg, err_type, do_raise=False, do_log=True, log_type=LogTypes.INFO):
+#     if err_type in _ERROR_TYPES:
+#       if do_log:
 
